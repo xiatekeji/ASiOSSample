ae9898cef7678ccde51d02e99fcb37a403890bb8
傅立业 <fuly@xiatekeji.com>
2015-11-18 13:46:23 +0800

    增加CommandCenter。

---
+++ ASAppFramework/ASCommandCenter/ASCommand.h
@@ -0,0 +1,13 @@
+//
+//  ASCommand.h
+//  ASIOSSample
+//
+//  Created by 傅立业 on 15/11/18.
+//  Copyright © 2015年 侠特科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASCommand : NSObject
+
+@end
---
+++ ASAppFramework/ASCommandCenter/ASCommandCenter.m
@@ -0,0 +1,13 @@
+//
+//  ASCommandCenter.m
+//  ASIOSSample
+//
+//  Created by 傅立业 on 15/11/18.
+//  Copyright © 2015年 XiaoSong. All rights reserved.
+//
+
+#import "ASCommandCenter.h"
+
+@implementation ASCommandCenter
+
+@end
