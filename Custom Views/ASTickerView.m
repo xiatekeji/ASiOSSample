0671504e049be92a5f26ac0ce0d3a935361a1977
傅立业 <fuly@xiatekeji.com>
2015-11-23 16:51:19 +0800

    加入已实现的ASTickerView（横向翻页）。

--- Custom Views/ASTickerView.m
+++ Custom Views/ASTickerView.m
@@ -12,7 +12,8 @@
 
 @interface ASTickerView() {
 	SBTickerView* _enclosedTickerView;
-	BOOL _rotated;
+	
+	BOOL _busy;
 }
 
 @end
@@ -27,6 +28,7 @@
 	SBTickerView* enclosedTickerView = [[SBTickerView alloc] init];
 	[enclosedTickerView setTransform: CGAffineTransformMakeRotation(M_PI_2)];
 	[enclosedTickerView setAutoresizingMask: UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight];
+	[enclosedTickerView setBackgroundColor: [UIColor yellowColor]];
 	[enclosedTickerView setFrame: [super bounds]];
 	[super addSubview: enclosedTickerView];
 	_enclosedTickerView = enclosedTickerView;
@@ -42,20 +44,18 @@
 	return self;
 }
 
-- (UIView*)frontView {
-	return [_enclosedTickerView frontView];
-}
-
 - (void)setFrontView: (UIView*)frontView {
-	[_enclosedTickerView setFrontView: frontView];
-}
-
-- (UIView*)backView {
-	return [_enclosedTickerView backView];
+	_frontView = frontView;
+	
+	UIView* wrapper = [self wrapperForView: frontView];
+	[_enclosedTickerView setFrontView: wrapper];
 }
 
 - (void)setBackView: (UIView*)backView {
-	[_enclosedTickerView setBackView: backView];
+	_backView = backView;
+	
+	UIView* wrapper = [self wrapperForView: backView];
+	[_enclosedTickerView setBackView: wrapper];
 }
 
 - (CFTimeInterval)duration {
@@ -67,6 +67,10 @@
 }
 
 - (void)tick: (ASTickerViewTickDirection)direction animated: (BOOL)animated completion: (void (^)(void))completion {
+	if(_busy) {
+		return;
+	}
+	
 	id<ASTickerViewDelegate> delegate = _delegate;
 	
 	BOOL shouldTick = TRUE;
@@ -78,12 +82,16 @@
 		return;
 	}
 	
+	_busy = TRUE;
+	
 	if([delegate respondsToSelector: @selector(tickerView:willTickWithDirection:)]) {
 		[delegate tickerView: self willTickWithDirection: direction];
 	}
 	
 	SBTickerViewTickDirection translatedDirection = [self translateDirection: direction];
 	[_enclosedTickerView tick: translatedDirection animated: animated completion: ^ {
+		_busy = FALSE;
+		
 		if([delegate respondsToSelector: @selector(tickerView:didTickWithDirection:)]) {
 			[delegate tickerView: self willTickWithDirection: direction];
 		}
@@ -92,6 +100,29 @@
 			completion();
 		}
 	}];
+}
+
+- (UIView*)wrapperForView: (UIView*)view {
+	// 由于SBTickerView在这里被顺时针旋转了90度，交给它的View必须事先逆时针旋转90度才能正确显示。
+	// 由于SBTickerView在进行动画时进行了View截图，所以交给它的View不能带有旋转效果。
+	
+	// 外层的Wrapper不进行旋转，并且保持和SBTickerView尺寸一致。
+	UIView* outerWrapper = [[UIView alloc] initWithFrame: [_enclosedTickerView bounds]];
+	[outerWrapper setAutoresizingMask: UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight];
+	[outerWrapper setClipsToBounds: TRUE];
+	
+	// 内层的Wrapper进行逆时针旋转90度，保持和ASTickerView尺寸一致。
+	UIView* innerWrapper = [[UIView alloc] init];
+	[innerWrapper setTransform: CGAffineTransformMakeRotation(- M_PI_2)];
+	[innerWrapper setAutoresizingMask: UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight];
+	[innerWrapper setClipsToBounds: TRUE];
+	[innerWrapper setFrame: [outerWrapper bounds]];
+	[innerWrapper layoutIfNeeded];
+	[outerWrapper addSubview: innerWrapper];
+	
+	[innerWrapper addSubview: view];
+	
+	return outerWrapper;
 }
 
 - (void)handleSwipe: (UISwipeGestureRecognizer*)swiper {
